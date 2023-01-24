8a9bf746d7485a0ff600f284404b87032d1bb1e1
Sandy Zhang <sandyzhang142@gmail.com>
2023-01-24 12:39:51 -0500

    added controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
-      end
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    private 
+    def render_unprocessable_entity_response(invalid)
+            render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+     end 
+  
+    def render_not_found_response(invalid)
+        render json: {error: "#{invalid.model} not found"}, status: :not_found
+     end 
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+    def index
+        render json: Post.all 
+    end
+
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
+    end
+
+    private 
+    def post_params
+        params.permit(:title, :description, :picture, :like)
+    end
 end
