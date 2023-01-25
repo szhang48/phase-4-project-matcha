a7279bc77c642d7089ed9583109d05d10467c7f6
Kamran Hussein <chinterswill@gmail.com>
2023-01-25 17:20:44 -0500

    This works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_161531) do
+ActiveRecord::Schema.define(version: 2023_01_25_200109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,17 +27,17 @@ ActiveRecord::Schema.define(version: 2023_01_24_161531) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
+    t.string "name"
+    t.string "content"
     t.string "picture"
-    t.integer "like"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "like"
-    t.integer "match"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.integer "post_id"
     t.integer "bio_id"
     t.datetime "created_at", precision: 6, null: false
