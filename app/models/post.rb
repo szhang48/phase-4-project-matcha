ed2d11d9c90564bcff6489bc05d94d92aee85edb
Sandy Zhang <sandyzhang142@gmail.com>
2023-01-25 23:53:08 -0500

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
     has_many :users, dependent: :destroy
-
     has_many :bios, through: :user 
 
-
+    validates :name, :content, :picture, presence: true
+    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :posts
-  resources :users
-  resources :bios
+  
+  resources :posts, only: [:index,:create]
+  resources :users, only: [:index,:create ]
+  resources :bios, only: [:index, :show, :create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
