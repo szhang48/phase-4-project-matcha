f55198ed1844bec21c4b3a8c637c6588c58c37a8
Samuel <lee.eunseong7856@gmail.com>
2023-01-26 21:12:21 -0600

    added status code to post index

---
+++ client/src/components/feed_components/SearchResultPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchResults from './SearchResults'
+
+function SearchResultPage({search}) {
+    console.log(search)
+    return (
+        <div className="search-result-page">
+            
+        </div>
+    )
+}
+
+export default SearchResultPage
