7897e67216e22eed2dfd4c78b245f04b2c525681
Samuel <lee.eunseong7856@gmail.com>
2023-01-27 10:22:07 -0600

    seed profile/cover photo rendering

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :password_digest
+  attributes :id, :name, :email, :password_digest, :profile_photo, :cover_photo, :birthday
 
+  belongs_to :post
 end
