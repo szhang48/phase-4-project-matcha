fed141b02768ac5402f21a8ba2680f536ce011e5
Samuel <lee.eunseong7856@gmail.com>
2023-01-27 11:35:17 -0600

    optimistic rendering for delete

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -7,9 +7,7 @@ function Nav() {
     return(
         <div className="nav">
             <div className="nav-item-container">
-                <nav className="nav-items">Feed</nav>
-                <nav className="nav-items">Profile</nav>
-                <nav className="nav-items">Standouts</nav>
+
             </div>
             <h2 className="title-logo">MATCHA</h2>
             <div className="login-signup-container">
